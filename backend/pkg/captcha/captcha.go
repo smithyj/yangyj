c6592a04f2af0d3bf1dfa6523319baedeff53b18
杨圆建 <624508914@qq.com>
2021-03-03 09:15:01 +0800

    调整验证码包

--- backend/pkg/captcha/captcha.go
+++ backend/pkg/captcha/captcha.go
@@ -21,27 +21,27 @@ type Captcha struct {
 func (c *Captcha) Image(w, h int) (img *captcha.Image, code string, err error) {
 	filename := "font/comic/comic.ttf"
 	bytes, err := assets.FS.ReadFile(filename)
-	cc := captcha.New()
+	draw := captcha.New()
 	if err == nil {
-		_ = cc.AddFontFromBytes(bytes)
+		_ = draw.AddFontFromBytes(bytes)
 	}
 	// 设置验证码大小
-	cc.SetSize(w, h)
+	draw.SetSize(w, h)
 	// 设置干扰强度
-	cc.SetDisturbance(captcha.HIGH)
+	draw.SetDisturbance(captcha.HIGH)
 	// 设置前景色 可以多个 随机替换文字颜色 默认黑色
-	cc.SetFrontColor(
+	draw.SetFrontColor(
 		color.RGBA{A: 255},
 		color.RGBA{R: 241, G: 60, B: 60, A: 255},
 		color.RGBA{R: 60, G: 81, B: 241, A: 255},
 	)
 
-	img, code = cc.Create(6, captcha.CLEAR)
-	err = c.Create(code)
+	img, code = draw.Create(6, captcha.CLEAR)
+	err = c.Write(code)
 	return
 }
 
-func (c *Captcha) Create(code string) (err error) {
+func (c *Captcha) Write(code string) (err error) {
 	err = redis.Redis.Set(fmt.Sprintf(PREFIX, c.id), code, 10*60*time.Second).Err()
 	return
 }
