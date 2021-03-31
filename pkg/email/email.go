2e050323553d97cc60a6921c79db41143ba82a49
杨圆建 <624508914@qq.com>
2021-03-31 10:15:25 +0800

    修复邮件发送中文发件人问题

--- pkg/email/email.go
+++ pkg/email/email.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gopkg.in/gomail.v2"
 	"math/rand"
+	"mime"
 	"yangyj/pkg/config"
 )
 
@@ -18,7 +19,7 @@ func (e *Email) send(mails ...*gomail.Message) (err error) {
 	for i, v := range mails {
 		from := cfg.Username
 		if cfg.Name != "" {
-			from = fmt.Sprintf("%v<%v>", cfg.Name, cfg.Username)
+			from = fmt.Sprintf("%v<%v>", mime.QEncoding.Encode("utf-8", cfg.Name), cfg.Username)
 		}
 		v.SetHeader("From", from)
 		m[i] = v
@@ -35,9 +36,10 @@ func New() *Email {
 func (e *Email) CaptchaCode(address, code string) (err error) {
 	m := gomail.NewMessage()
 	m.SetHeader("To", address)
-	m.SetHeader("Subject", fmt.Sprintf("验证码: %v", code))
+	subject := fmt.Sprintf("验证码 %v", code)
+	m.SetHeader("Subject", subject)
 	expired := config.Config.Captcha.Expired
-	body := fmt.Sprintf("%v分钟内有效，请勿泄露给他人！", expired)
+	body := fmt.Sprintf("%v， %v分钟内有效，请勿泄露给他人！", subject, expired)
 	m.SetBody("text/html", body)
 
 	return e.send(m)
