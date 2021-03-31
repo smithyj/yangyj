e5ade15ea8bca159645893bede86c0598dcbb340
杨圆建 <624508914@qq.com>
2021-03-31 13:58:36 +0800

    改进UUID方法，防止随机碰撞

--- internal/handler/uuid/uuid.go
+++ internal/handler/uuid/uuid.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/satori/go.uuid"
 	"net/http"
+	"strconv"
+	"time"
 	"yangyj/internal/handler"
 	"yangyj/pkg/e"
 )
@@ -15,6 +17,7 @@ type Handler struct{
 func (handler *Handler) UUID() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		u := uuid.NewV4()
+		u = uuid.NewV5(u, strconv.Itoa(time.Now().Nanosecond()))
 		s := u.String()
 
 		handler.JSON(ctx, http.StatusOK, gin.H{
